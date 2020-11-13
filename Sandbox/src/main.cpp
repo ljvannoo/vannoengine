76e75bbe543dc849b13412b980c2751bd40b45c2
Lukas VanNoord <lukas.vannoord@gmail.com>
2020-11-13 10:52:33 -0800

    Move application configuration into a configuration manager

--- Sandbox/src/main.cpp
+++ Sandbox/src/main.cpp
@@ -3,14 +3,11 @@
 #include "engine/systems/objects/GameObjectFactory.h"
 #include "engine/systems/InputManager.h"
 #include "engine/components/ComponentCreator.h"
+#include "engine/systems/ConfigurationManager.h"
 
 #include "engine/components/Camera.h"
 
 #include "Controller.h"
-
-#define WINDOW_WIDTH 800
-#define WINDOW_HEIGHT 600
-#define FPS_CAP 60
 
 class Sandbox : public VannoEngine::Game {
 public:
@@ -23,24 +20,22 @@ public:
 	}
 
 	void Init() {
-		VannoEngine::Game::Init(WINDOW_WIDTH, WINDOW_HEIGHT, FPS_CAP);
+		VannoEngine::ConfigurationManager* pConfigurationManager = VannoEngine::ConfigurationManager::GetInstance();
+		pConfigurationManager->Init("config.json");
+
+		int windowWidth = pConfigurationManager->GetInt("/window/width");
+		int windowHeight = pConfigurationManager->GetInt("/window/height");
+
+		VannoEngine::Game::Init(windowWidth, windowHeight, pConfigurationManager->GetInt("/maxFrameRate"));
 		
 		// Add custom components
 		VannoEngine::GameObjectFactory* pObjectFactory = VannoEngine::GameObjectFactory::GetInstance();
 		pObjectFactory->RegisterComponent(CONTROLLER_COMPONENT, new VannoEngine::ComponentCreator<Controller>());
 
-		// Register keys
-		VannoEngine::InputManager* pInputManager = VannoEngine::InputManager::GetInstance();
-		pInputManager->RegisterAction(VannoEngine::Action::MOVE_UP, VannoEngine::Key::W);
-		pInputManager->RegisterAction(VannoEngine::Action::MOVE_DOWN, VannoEngine::Key::S);
-		pInputManager->RegisterAction(VannoEngine::Action::MOVE_LEFT, VannoEngine::Key::A);
-		pInputManager->RegisterAction(VannoEngine::Action::MOVE_RIGHT, VannoEngine::Key::D);
-		pInputManager->RegisterAction(VannoEngine::Action::JUMP, VannoEngine::Key::SPACE);
-
 		// Load the level
 		VannoEngine::LevelManager* pLevelManager = VannoEngine::LevelManager::GetInstance();
-		pLevelManager->LoadLevel("levels\\demo_level1.json");
-		pLevelManager->GetCamera()->SetScreenDimensions(WINDOW_WIDTH, WINDOW_HEIGHT);
+		pLevelManager->LoadLevel(pConfigurationManager->GetString("/initialLevel"));
+		pLevelManager->GetCamera()->SetScreenDimensions(windowWidth, windowHeight);
 	}
 };
 
