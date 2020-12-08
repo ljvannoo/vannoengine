7904ed62ebed9841b3f487331c50678d0b3c5ace
Lukas VanNoord <lukas.vannoord@gmail.com>
2020-12-08 13:10:05 -0800

    Change name of the engine logger

--- VannoEngine/src/engine/core/Log.cpp
+++ VannoEngine/src/engine/core/Log.cpp
@@ -9,7 +9,7 @@ namespace VannoEngine {
 	void Log::Init() {
 		spdlog::set_pattern("%^[%T] %n: %v%$");
 
-		sCoreLogger = spdlog::stdout_color_mt("VannoEngine");
+		sCoreLogger = spdlog::stdout_color_mt("Engine");
 		sCoreLogger->set_level(spdlog::level::trace);
 		sClientLogger = spdlog::stdout_color_mt("Game");
 		sClientLogger->set_level(spdlog::level::trace);
